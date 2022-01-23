bf434fd00e0b920726492fa7c6f5df785e628913
levan sardalashvili <sardalaalevani@gmail.com>
2022-01-23 16:54:38 +0400

    Updates

---
+++ precache-manifest.ca38a09251f5e09d9adc5b0d2ba551b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f351420d34d747370896f466e0689f3",
+    "url": "/react-search-image-app/index.html"
+  },
+  {
+    "revision": "5ad14415bc6117518004",
+    "url": "/react-search-image-app/static/css/main.30f1b11e.chunk.css"
+  },
+  {
+    "revision": "08b3c966dab06e4190cd",
+    "url": "/react-search-image-app/static/js/2.d26f9645.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/react-search-image-app/static/js/2.d26f9645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad14415bc6117518004",
+    "url": "/react-search-image-app/static/js/main.a8167e57.chunk.js"
+  },
+  {
+    "revision": "de940d1f14f31506eeff",
+    "url": "/react-search-image-app/static/js/runtime-main.5f55a3ba.js"
+  }
+]);
